b40eb889c558bb04e3a32379acafc6f6d305d66d
Adam Tiesman <adam.j.tiesman@vanderbilt.edu>
2024-10-11 23:27:57 -0500

    coherence set changing

--- Assets/_Scripts/BlockGenerator.cs
+++ Assets/_Scripts/BlockGenerator.cs
@@ -7,7 +7,7 @@ public class BlockGenerator : MonoBehaviour
 {
     // List possible trial conditions. Initially using 2-AFC left-or-right motion discrimination task.
     private List<TrialDef> trialDefs;
-    public float[] coherences = new float[]{0.05f, .1f, .15f, .2f, .4f};
+    public float[] coherences = new float[]{0.02f, 0.05f, 0.07f, 0.1f, 0.15f, 0.2f, 0.4f};
     public int[] directions = new int[]{1, -1};
     // public int[] y_directions = new int[]{1, -1};
     // public int[] z_directions = new int[]{1, -1};
